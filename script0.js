51392b4ce737ccf87b3671410db00552c91242b4
dEnchanter <afolabiopaleye@gmail.com>
2020-08-02 16:44:08 -0400

    first commit

---
+++ script0.js
@@ -0,0 +1,6 @@
+function myDetails(name, email, lang, stack, userName){
+    //print name, email, language used, biostack, slack username with @
+    console.log(`My details are: ${name}, ${email}, ${lang}, ${stack}, @${userName}`);
+}
+
+myDetails('FOla Utthy', 'folautthy@gmail.com', 'lang', 'Software Developer', 'fola')
